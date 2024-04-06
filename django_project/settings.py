023315fe391096501e360f27e3d14a6643ec075f
Zachary Rytting <zrytting@uccs.edu>
2024-04-06 11:49:32 -0600

    Sprint01

--- django_project/settings.py
+++ django_project/settings.py
@@ -9,9 +9,8 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
-from pathlib import Path
 import os
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2=3_j(5gu626v5*#i!bh5m-@2#3+w8@bgq(_h9w*6r3v-4_#kw'
+SECRET_KEY = 'django-insecure-92ug1nhf!(+7%t-pwhob^*hone5qde*$##nbx_sffy1h1k3t7h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'portfolio_app',
+    'piano_project',
 ]
 
 AUTHENTICATION_BACKENDS = [
@@ -121,13 +120,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+os.path.join(BASE_DIR, 'static')
 ]
 
-MEDIA_URL = '/images/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
