91d0519fcaf9cffa3b0dc7e6af540726d4305117
Zachary Rytting <zrytting@uccs.edu>
2024-04-18 14:28:31 -0600

    Role Authentication

--- piano_project/urls.py
+++ piano_project/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
@@ -9,5 +9,8 @@ path('', views.index, name='index'),
 path('<slug:slug>/details', views.teacherDetailView.as_view(), name='teacher_details'),
 path('teacher/create', views.createTeacher, name='create_teacher'),
 path('teacher/update/<slug:slug>', views.updateTeacher, name='update_teacher'),
-path('teacher/delete/<slug:slug>', views.deleteTeacher, name='delete_teacher')
+path('teacher/delete/<slug:slug>', views.deleteTeacher, name='delete_teacher'),
+path('accounts/', include('django.contrib.auth.urls')),
+path('accounts/register/', views.registerPage, name='register_page')
+#path('user/', views.userPage, name='user_page')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
